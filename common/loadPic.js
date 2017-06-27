a23ad7eb1e6c115b0579031425dda44ed5c53cb9
Shawn Phang <xiao@pengxiaopengdeMacBook-Air.local>
2017-06-27 21:02:22 +0800

    增加起始页背景图

--- common/loadPic.js
+++ common/loadPic.js
@@ -1,6 +1,6 @@
 //预加载图片方法
 
-var pro = ['images/background.jpeg','images/starfish-middle0.png','images/starfish-middle1.png','images/starfish-middle2.png','images/starfish-middle3.png','images/starfish-middle4.png','images/starfish-middle5.png','images/starfish-middle6.png','images/starfish-middle7.png','images/starfish-middle8.png','images/starfish-middle9.png','images/starfish-small.png','images/enemy/blowfish-icon.png','images/enemy/crab-icon-middle.png','images/enemy/octo-icon.png','images/enemy/squid-icon-middle.png'], // 预加载图片路径
+var pro = ['images/background.jpeg','images/starfish-middle0.png','images/starfish-middle1.png','images/starfish-middle2.png','images/starfish-middle3.png','images/starfish-middle4.png','images/starfish-middle5.png','images/starfish-middle6.png','images/starfish-middle7.png','images/starfish-middle8.png','images/starfish-middle9.png','images/enemy/blowfish-icon.png','images/enemy/crab-icon-middle.png','images/enemy/octo-icon.png','images/enemy/squid-icon-middle.png'], // 预加载图片路径
 	proTip = ['正在疯狂布置海洋...','主角急匆匆进入海洋...','主角正在变装...','白色智障进入海洋...','紫色奇葩进入海洋...','红色大佬正在路上...','Boss章鱼暗中观察...'], // 预加载图片路径
 	i = 0,
 	timer = null,
